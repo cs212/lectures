28da920e46aef3c31f942082c3b1c2e3286d94d5
Sophie Engle <sjengle@cs.usfca.edu>
2014-11-01 19:54:50 -0700

    Initializing Jetty demos.

---
+++ DynamicHTML/README.md
@@ -0,0 +1,22 @@
+Dynamic HTML
+=================================================
+
+These demos illustrate some basics on dynamic HTML using servlets. 
+
+*Note that these examples are designed to illustrate specific concepts, and may not be an example of proper class design outside of this context.*
+
+## Relevant Resources ##
+
+To add Jetty as a third-party library in Eclipse, you will need the aggregate `jar` files located at the following links:
+
+- [`jetty-all-9.x.x.jar`](http://central.maven.org/maven2/org/eclipse/jetty/aggregate/jetty-all/)
+- [`javax.servlet-3.x.x.jar`](http://central.maven.org/maven2/javax/servlet/javax.servlet-api/)
+
+Replace each `x` with the latest stable version numbers. For help with embedded Jetty, see:
+
+- [Jetty Development Guide - Embedding](http://www.eclipse.org/jetty/documentation/current/advanced-embedding.html)
+
+## Relevant Lectures ##
+
+Please see the [course website](http://cs212.cs.usfca.edu) for links to lecture slides and videos.
+
---
+++ DynamicHTML/src/VisitServer.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+/**
+ * A simple example of using Jetty and servlets to track the number of
+ * visitors to a web page, demonstrating that this code is run in a multi-
+ * threaded setting.
+ */
+public class VisitServer {
+
+	public static final int PORT = 8080;
+	private static int visits = 0;
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+
+		ServletHandler handler = new ServletHandler();
+		handler.addServletWithMapping(VisitServlet.class, "/");
+
+		server.setHandler(handler);
+		server.start();
+		server.join();
+	}
+
+	@SuppressWarnings("serial")
+	public static class VisitServlet extends HttpServlet {
+
+		private static final String TITLE = "Visits";
+
+		@Override
+		protected void doGet(
+				HttpServletRequest request,
+				HttpServletResponse response)
+				throws ServletException, IOException {
+
+			// Check to make sure the browser is not requesting favicon.ico
+			if (request.getRequestURI().endsWith("favicon.ico")) {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+				return;
+			}
+
+			response.setContentType("text/html");
+
+			PrintWriter out = response.getWriter();
+			out.printf("<html>%n");
+			out.printf("<head><title>%s</title></head>%n", TITLE);
+			out.printf("<body>%n");
+
+			// Safely increment the number of visits to this website
+			synchronized (VisitServer.class) {
+				visits++;
+				out.printf("<p>There have been %d visits to this page.%n", visits);
+			}
+
+			// Demonstrate that this servlet is called by different threads
+			out.printf("<p>This request was handled by thread %s.</p>%n",
+					Thread.currentThread().getName());
+
+			out.printf("</body>%n");
+			out.printf("</html>%n");
+
+			response.setStatus(HttpServletResponse.SC_OK);
+		}
+	}
+}
